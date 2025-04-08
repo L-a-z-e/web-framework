183dd99a24dd4152c4d2f987f8e6ec71b5f73f63
L-a-z-e <yysi8771@gmail.com>
2025-04-08 23:17:32 +0900

    db driver dependencies 임시 조치

--- backend/src/main/java/com/laze/backend/BackendApplication.java
+++ backend/src/main/java/com/laze/backend/BackendApplication.java
@@ -2,8 +2,13 @@ package com.laze.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    })
 public class BackendApplication {
 
 	public static void main(String[] args) {
